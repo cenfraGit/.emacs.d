6af4b9606d1ef99ae8adba4caa9b763a7acde714
cenfra <cenfra@localhost.localdomain>
2025-01-31 18:46:34 -0500

    updated

--- init.el
+++ init.el
@@ -24,26 +24,6 @@
  truncate-lines t
  )
 
-(setq-default message-log-max nil)
-(kill-buffer "*Messages*")
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-(use-package doom-themes
-  :config
-  (let ((chosen-theme 'doom-material-dark))
-    (doom-themes-visual-bell-config)
-    (doom-themes-org-config)
-    (setq doom-challenger-deep-brighter-comments t
-          doom-challenger-deep-brighter-modeline t
-          doom-rouge-brighter-comments t
-          doom-ir-black-brighter-comments t
-          modus-themes-org-blocks 'gray-background
-          doom-dark+-blue-modeline nil)
-    (load-theme chosen-theme)))
-(use-package multiple-cursors)
-
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (tooltip-mode -1)
@@ -51,9 +31,10 @@
 (global-display-line-numbers-mode 1)
 (delete-selection-mode t)
 
-(set-frame-font "Cascadia Code 11" nil t)
-(add-to-list 'default-frame-alist '(width . 70))
-(add-to-list 'default-frame-alist '(height . 25))
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+
+(set-frame-font "NanumGothicCoding Bold" nil t)
 
 (load-file "~/.emacs.d/functions.el")
 (global-set-key (kbd "C-c c") 'insert-comment-based-on-mode)
@@ -63,7 +44,107 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-(defun load-setup()
-  (interactive)
-  (load-file "~/.emacs.d/setup.el"))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; -------------------- package setup -------------------- ;;
+
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; ----------------------- packages ----------------------- ;;
+
+;; (use-package doom-themes
+;;   :config
+;;   (let ((chosen-theme 'doom-material-dark))
+;;     (doom-themes-visual-bell-config)
+;;     (doom-themes-org-config)
+;;     (setq doom-challenger-deep-brighter-comments t
+;;           doom-challenger-deep-brighter-modeline t
+;;           doom-rouge-brighter-comments t
+;;           doom-ir-black-brighter-comments t
+;;           modus-themes-org-blocks 'gray-background
+;;           doom-dark+-blue-modeline nil)
+;;     (load-theme chosen-theme)))
+
+(use-package catppuccin-theme
+  :ensure t
+  :config
+  (load-theme 'catppuccin t))
+
+
+(use-package multiple-cursors)
+
+;; (setq package-check-signature nil)
+
+(unless (package-installed-p 'lsp-mode)
+  (package-refresh-contents)
+  (package-install 'lsp-mode))
+
+(unless (package-installed-p 'lsp-pyright)
+  (package-refresh-contents)
+  (package-install 'lsp-pyright))
+
+(use-package lsp-mode
+  :ensure t
+  :hook ((python-mode . lsp-deferred))
+  :commands (lsp lsp-deferred)
+  :config
+  (setq lsp-prefer-flymake nil))
+
+(use-package lsp-pyright
+  :ensure t
+  :after lsp-mode
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pyright)
+                         (lsp-deferred))))
+
+(setq lsp-log-io t)
+(setq lsp-server-trace t)
+
+(use-package tree-sitter)
+(use-package tree-sitter-langs)
+(use-package company)
+(use-package eldoc-box)
+
+(use-package dired-sidebar
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar))
+
+(use-package visual-regexp
+  :bind (("C-c 5" . #'vr/replace)))
+
+;; (let ((installed (package-installed-p 'all-the-icons)))
+;;   (use-package all-the-icons)
+;;   (unless installed (all-the-icons-install-fonts)))
+
+;; (use-package all-the-icons-dired
+;;   :after all-the-icons
+;;   :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package dired
+  :ensure nil
+  :config
+  (setq delete-by-moving-to-trash t)
+  (eval-after-load "dired"
+    #'(lambda ()
+        (put 'dired-find-alternate-file 'disabled nil)
+        (define-key dired-mode-map (kbd "RET") #'dired-find-alternate-file))))
+
+;; ----------------------- commands ----------------------- ;;
+
+(global-tree-sitter-mode)
+(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+(add-hook 'prog-mode-hook 'hs-minor-mode)
+(global-set-key (kbd "C-c v") 'dired-sidebar-toggle-sidebar)
 
