bf7fa27b4c457fa3bd447ac572e87f69af3e3f25
cenfraGit <161994865+cenfraGit@users.noreply.github.com>
2026-02-02 18:02:26 -0600

    update init.el

--- init.el
+++ init.el
@@ -1,30 +1,30 @@
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(setq gc-cons-threshold (* 50 1024 1024))
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 2 1024 1024))))
+
 ;; --------------------------------------------------------------------------------
 ;; package setup
 ;; --------------------------------------------------------------------------------
 
-(require 'use-package)
-(setq use-package-always-ensure t)
-
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("gnu" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
+                         ("org"  . "https://orgmode.org/elpa/")
+                         ("gnu"  . "https://elpa.gnu.org/packages/")))
+
+;; (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(require 'use-package)
+(eval-when-compile
+  (require 'use-package))
 (setq use-package-always-ensure t)
-(setq package-check-signature nil)
-
-(unless (package-installed-p 'gnu-elpa-keyring-update)
-  (package-refresh-contents)
-  (package-install 'gnu-elpa-keyring-update))
-
-(unless package-archive-contents
-  (package-refresh-contents))
 
 ;; --------------------------------------------------------------------------------
 ;; packages
@@ -39,25 +39,24 @@
         (put 'dired-find-alternate-file 'disabled nil)
         (define-key dired-mode-map (kbd "RET") #'dired-find-alternate-file))))
 
-(use-package multiple-cursors)
+(use-package multiple-cursors
+  :bind (("C-c x" . mc/edit-lines)
+         ("C->"   . mc/mark-next-like-this)
+         ("C-<"   . mc/mark-previous-like-this)
+         ("C-c C-<" . mc/mark-all-like-this)))
 
 (use-package company
-  :hook (after-init . global-company-mode)
+  :defer 2
   :custom
   (company-idle-delay 0.2)
   (company-minimum-prefix-length 1)
-  (company-tooltip-align-annotations t))
-
-(use-package magit
-  :commands (magit-status magit-get-current-branch)
   :config
-  (setq magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+  (global-company-mode 1))
 
 (use-package treemacs
   :defer t
-  :config
+  :init (setq treemacs-width 42)
   :bind (("C-c v" . treemacs)))
-(setq treemacs-width 42)
 
 (use-package doom-themes
   :ensure t
@@ -68,20 +67,6 @@
   :config
   (doom-themes-visual-bell-config)
   (doom-themes-org-config))
-
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook ((csharp-mode . lsp-deferred))
-  :commands lsp lsp-deferred)
-
-(use-package lsp-ui
-  :ensure t
-  :config
-  (setq lsp-ui-doc-enable t)
-  (setq lsp-ui-doc-show-with-cursor t)
-  (setq lsp-ui-doc-position 'at-point)
-  :hook (lsp-mode . lsp-ui-mode))
 
 ;; --------------------------------------------------------------------------------
 ;; variables
@@ -135,10 +120,6 @@
 (add-to-list 'default-frame-alist '(width . 100))
 (add-to-list 'default-frame-alist '(height . 30))
 
-(add-to-list 'custom-theme-load-path (expand-file-name
-                                      "themes"
-                                      user-emacs-directory))
-
 ;; --------------------------------------------------------------------------------
 ;; commands
 ;; --------------------------------------------------------------------------------
@@ -155,34 +136,34 @@
 (global-auto-revert-mode 1)
 (global-visual-line-mode 1)
 (scroll-bar-mode -1)
-(tooltip-mode -1)
 (delete-selection-mode t)
+(tooltip-mode -1)
 
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-
-;; encoding
-(set-language-environment 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-keyboard-coding-system 'utf-8-unix)
-(set-terminal-coding-system 'utf-8-unix)
+;; (tool-bar-mode -1)
+;; (menu-bar-mode -1)
 
 ;; functions and commands
 (load-file "~/.emacs.d/functions.el")
 (global-set-key (kbd "C-c c") 'my/insert-inline-comment-based-on-mode)
 (global-set-key (kbd "C-c d") 'my/insert-block-comment)
 (global-set-key (kbd "C-;") 'comment-dwim)
-(global-set-key (kbd "C-c x") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-c 5") #'search-forward-regexp)
 
 ;; csharp
 (defun csharp-mode-setup ()
   (setq c-basic-offset 4)
   (setq tab-width 4)
   (setq indent-tabs-mode nil))
+
+(define-generic-mode 'deltascript-mode
+  '("//" ("/*" . "*/"))
+  '("if" "else" "while" "break"
+    "continue" "func" "return")
+  '(("\\([a-zA-Z_][a-zA-Z0-9_]*\\)(" . (1 font-lock-function-name-face))
+    ("\\b[0-9]+\\(\\.[0-9]+\\)?\\b" . font-lock-constant-face)
+    ("=>" . font-lock-keyword-face))
+  '("\\.ds$")
+  nil
+  "Major mode for DeltaScript files.")
 
 ;; --------------------------------------------------------------------------------
 ;; hooks
@@ -208,12 +189,16 @@
 ;; (load-theme 'spacemacs-dark t)
 ;; (load-theme 'leuven t)
 
-;; (add-to-list 'default-frame-alist '(font . "Courier-10"))
+;; --------------------------------------------------------------------------------
+;; text
+;; --------------------------------------------------------------------------------
+
 (add-to-list 'default-frame-alist '(font . "Lucida Console-10"))
+;; (add-to-list 'default-frame-alist '(font . "Courier-10"))
 ;; (add-to-list 'default-frame-alist '(font . "Iosevka-10"))
 
-;; --------------------------------------------------------------------------------
-;; start in org mode (daemon)
-;; --------------------------------------------------------------------------------
-
-(org-mode)
+;; encoding
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
